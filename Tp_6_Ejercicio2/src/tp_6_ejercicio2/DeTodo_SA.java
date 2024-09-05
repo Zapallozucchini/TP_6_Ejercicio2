4abaefbe5cd40b1e67507fb8bb91e18f8c3e9e5b
Zapallozucchini <juan.clavero24@gmail.com>
2024-09-05 19:25:29 -0300

    Listado por precio InternalFRAME
    hice el listadoporprecio con una internal frame y agregue que se pueda ver en el panel original

--- Tp_6_Ejercicio2/src/tp_6_ejercicio2/DeTodo_SA.java
+++ Tp_6_Ejercicio2/src/tp_6_ejercicio2/DeTodo_SA.java
@@ -76,6 +76,11 @@ public class DeTodo_SA extends javax.swing.JFrame {
         jMConsulta.add(jMIConsultaNombre);
 
         jMIConsultaPrecio.setText("Consulta por Precio");
+        jMIConsultaPrecio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMIConsultaPrecioActionPerformed(evt);
+            }
+        });
         jMConsulta.add(jMIConsultaPrecio);
 
         jMIConsultaRubro.setText("Consulta por Rubro");
@@ -109,6 +114,14 @@ public class DeTodo_SA extends javax.swing.JFrame {
         escritorio.add(gestionProducto);
         gestionProducto.setVisible(true);
     }//GEN-LAST:event_jMIGestionDeProductoActionPerformed
+
+    private void jMIConsultaPrecioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMIConsultaPrecioActionPerformed
+        ListadoPorPrecio listadoPorPrecio = new ListadoPorPrecio();
+        escritorio.add(listadoPorPrecio);
+        listadoPorPrecio.setVisible(true);
+        
+        
+    }//GEN-LAST:event_jMIConsultaPrecioActionPerformed
 
     /**
      * @param args the command line arguments
